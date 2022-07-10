5a753a79839085620f94d35c0472719ab56ae940
McKay Beckstead <bec21017@byui.edu>
2022-07-09 21:23:16 -0600

    added a movedown and bounce all aliens class

--- Game/Casting/Alien.cs
+++ Game/Casting/Alien.cs
@@ -50,6 +50,20 @@ namespace cse210_10.Game.Casting
         /// <summary>
         /// Negates current velocity in the x direction.
         /// </summary>
+        public void BounceXForEach(Cast cast)
+        {
+            List<Actor> aliens = cast.GetActors(Constants.BRICK_GROUP);
+            foreach (Alien alien in aliens)
+            {
+
+            Point velocity = body.GetVelocity();
+            int vx = velocity.GetX() * -1;
+            int vy = velocity.GetY();
+            Point newVelocity = new Point((int)vx, (int)vy);
+            body.SetVelocity(newVelocity);
+            }
+
+        }
         public void BounceX()
         {
             Point velocity = body.GetVelocity();
@@ -66,6 +80,16 @@ namespace cse210_10.Game.Casting
             Point velocity = body.GetVelocity();
             Point newVelocity = new Point(Constants.ALIEN_X_VELOCITY, 0);
             body.SetVelocity(newVelocity);
+        }
+
+        public void moveDown()
+        {
+            
+            Point oldposition = body.GetPosition();
+            int x = oldposition.GetX();
+            int y = oldposition.GetY();
+            Point newPosition = new Point(x, y + Constants.BRICK_HEIGHT);
+            body.SetPosition(newPosition);
         }
     }
 }
